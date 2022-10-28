aacea42b14e619425fbe348d202194aa5767829c
Anthony <anthony.rabot@gmail.com>
2022-10-28 23:53:34 +0200

    WIP home page

--- front/js/script.js
+++ front/js/script.js
@@ -9,7 +9,50 @@ fetch('http://localhost:3000/api/products', {
             return result.json();
         }
     })
-    .then(result => console.log(result))
+    // Appeler une Méthode qui va construire le HTML
+    .then(
+        products => displayProducts(products)
+    )
     .catch((erreur) => {
-        console.log(`Erreur avec le message : ${erreur}`)
+        console.log(`Erreur lors de la récupération des produits avec le message : ${erreur}`)
     });
+
+
+/**
+ * Parse Products and create HTML tags for each one
+ * @param {Response} products - Array of products objects
+ */
+const displayProducts = (products) => {
+
+    const parent = document.getElementById("items");
+
+    for (const product of products) {
+
+        // Create Link
+        const link = document.createElement('a')
+        link.href = `product.html?id=${product._id}`;
+        parent.appendChild(link);
+
+        // Create Article
+        const article = document.createElement('article')
+        link.appendChild(article);
+
+        // Create Image
+        const image = document.createElement('img')
+        image.src = product.imageUrl
+        image.alt = product.altTxt
+        article.appendChild(image);
+
+        // Create Title
+        const title = document.createElement('h3')
+        title.textContent = product.name
+        title.classList.add('productName')
+        article.appendChild(title);
+
+        // Create Description
+        const description = document.createElement('p')
+        description.classList.add('productDescription')
+        description.textContent = product.description
+        article.appendChild(description);
+    }
+}
